7cff7005e11aeed1fabce433a26da58c9599a778
Didier Vervier <2205137vervier@zuyd.nl>
2023-04-16 14:09:46 +0200

    ,

--- DAL.cs
+++ DAL.cs
@@ -11,7 +11,7 @@ namespace FeedBuff_Kerkrade
 {
     public class DAL
     {
-        string connectionString = "Data Source=.;Initial Catalog=Feedbuf;Integrated Security=True";
+        string connectionstring = "Data Source=.;Initial Catalog=Feedbuf;Integrated Security=True";
         public void User_Insert_database(User user)
         {
             try
@@ -91,5 +91,84 @@ namespace FeedBuff_Kerkrade
             }
         }
 
+        public void LoadSubjectsIntoComboBox(ComboBox comboBox)
+        {
+            // Establish a connection to your database
+            using (SqlConnection connection = new SqlConnection(connectionstring))
+            {
+                // Open the connection
+                connection.Open();
+
+                // Define the SQL query to select the Subject_name column from the Subject table
+                string query = "SELECT Subject_name FROM Subject";
+
+                // Create a SqlCommand object to execute the SQL query
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    // Create a SqlDataReader object to read the results of the SQL query
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        // Clear the existing items in the ComboBox
+                        comboBox.Items.Clear();
+
+                        // Loop through each record in the SqlDataReader and add the Subject_name value to the ComboBox
+                        while (reader.Read())
+                        {
+                            string subjectName = reader.GetString(0);
+                            comboBox.Items.Add(subjectName);
+                        }
+                    }
+                }
+
+                // Close the connection
+                connection.Close();
+            }
+        }
+        public void LoadTeachersIntoCombobox(ComboBox comboBox)
+        {
+            // Establish a connection to your database
+            using (SqlConnection connection = new SqlConnection(connectionstring))
+            {
+                // Open the connection
+                connection.Open();
+
+                // Define the SQL query to select the names of all users where Role_ID equals 2
+                string query = "SELECT Name FROM [User] WHERE Role_ID = 2";
+
+                // Create a SqlCommand object to execute the SQL query
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    // Create a SqlDataReader object to read the results of the SQL query
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        // Clear the existing items in the ComboBox
+                        comboBox.Items.Clear();
+                        comboBox.Items.Add("");
+
+                        // Loop through each record in the SqlDataReader and add the name value to the ComboBox
+                        while (reader.Read())
+                        {
+                            string userName = reader.GetString(0);
+                            comboBox.Items.Add(userName);
+                        }
+                    }
+                }
+
+                // Close the connection
+                connection.Close();
+            }
+        }
+        public void LoadWeekIntoComboBox(ComboBox comboBox)
+        {
+            // Clear the existing items in the ComboBox
+            comboBox.Items.Clear();
+            comboBox.Items.Add("");
+
+            // Loop from 1 to 10 and add each integer value with "Week" string to the ComboBox
+            for (int i = 1; i <= 10; i++)
+            {
+                comboBox.Items.Add("Week " + i);
+            }
+        }
     }
 }
