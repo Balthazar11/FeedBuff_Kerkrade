51913993a81c658b3c44d5490586219575aa7fe7
Didier Vervier <2205137vervier@zuyd.nl>
2023-04-16 14:15:33 +0200

    .

--- DAL.cs
+++ DAL.cs
@@ -91,7 +91,84 @@ namespace FeedBuff_Kerkrade
                 comboBox.Items.Add("Week " + i);
             }
         }
+        public void User_Insert_database(User user)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionstring))
+                {
+                    string sql = "INSERT INTO [User] (Name, Password) VALUES (@Name, @Password)";
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        command.Parameters.AddWithValue("@Name", user.Name);
+                        command.Parameters.AddWithValue("@Password", user.Password);
 
+                        // Open de databaseverbinding en voer de SQL-query 
+                        int result = command.ExecuteNonQuery();
+
+                        // Controleer het resultaat
+                        if (result > 0)
+                        {
+                            MessageBox.Show("Record inserted successfully");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Record not inserted");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error connecting to database: " + ex.Message);
+            }
+
+        }
+
+        public bool User_Check_database(User user)
+        {
+            try
+            {
+                // lijsten voor de naam en wachtwoord uit de database
+                List<string> usernames = new List<string>();
+                List<string> passwords = new List<string>();
+
+                string query = "SELECT Name, Password FROM [User]";
+
+                using (SqlConnection connection = new SqlConnection(connectionstring))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                usernames.Add(reader.GetString(0));
+                                passwords.Add(reader.GetString(1));
+                            }
+                        }
+                    }
+                }
+
+                // Checken of de name met het gebruikte password overeenkomt
+                for (int i = 0; i < usernames.Count; i++)
+                {
+                    if (user.Name == usernames[i] && user.Password == passwords[i])
+                    {
+                        return true;
+                    }
+                }
+                return false;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error connecting to database: " + ex.Message);
+                return false;
+            }
+        }
 
     }
 }
