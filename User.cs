40ea66da4a3b248ef99db1e4510e792a64fb4f29
Didier Vervier <2205137vervier@zuyd.nl>
2023-04-16 14:20:07 +0200

    .

--- User.cs
+++ User.cs
@@ -10,8 +10,24 @@ namespace FeedBuff_Kerkrade
     public class User
     {
         public int User_ID { get; set; }
-        public int Role_ID { get; set; }
+        public Role Role { get; set; }
         public string Name { get; set; }
         public string Password { get; set; }
+
+        public User(int User_ID, Role role, string Name, string Password)
+        {
+            this.User_ID = User_ID;
+            this.Role = role;
+            this.Name = Name;
+            this.Password = Password;
+        }
+
+        public User(string Name, string Password)
+        {
+            this.User_ID = -1;
+            this.Role = null;
+            this.Name = Name;
+            this.Password = Password;
+        }
     }
 }
